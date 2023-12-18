387c0b9ae4021ad4687c3c749057a522d616913a
brai-lrnz <elibraibel@gmail.com>
2023-12-18 15:01:29 -0400

    Dia 3

--- scripts-sh/pipes.sh
+++ scripts-sh/pipes.sh
@@ -11,9 +11,15 @@ read st1
 echo "Segundo Texto"
 read st2
 
-if [ "$st1" == "$st2" ]
+if [ "$st1" \< "$st2" ]
 then
-	echo "Los textos coinciden"
+	echo "$st1 tiene menos letras que $st2"
+
+elif [ "$st1" \< "$st2" ]
+then
+	echo "$st2 tiene mas letras que $st1"
 else
-	echo "Los textos no coinciden"
+	echo "Son iguales"
 fi
+
+#Other Syntax -> if [ "$st1" == "$st2" ]
