04a2acce216ca7ad1d9a6b5575815f88407cf6c1
brai-lrnz <elibraibel@gmail.com>
2024-01-23 13:03:50 -0400

    Dia 9.0.1

---
+++ python-codes/exc1.py
@@ -0,0 +1,8 @@
+# Primer Programa
+
+print("Saludos!! Bienvenidos a mi programa")
+
+pet = input("Ingresa el nombre de tu mascota: ")
+city = input("Ingresa tu ciudad: ")
+
+print("Your new twitter handle and bio @cyber"+ pet + " " + "from",city)
--- python-codes/hello.py
+++ python-codes/hello.py
@@ -1,5 +1,39 @@
 
-
+# Imprimir cualquier string por consola
 print("hello world 1")
 print("hello world 2")
 print("Hello World 3")
+
+# imprimir 2 o mas strings con espacios
+print("Yooo" + " chico")
+print("Yooo" + " " + "chico")
+
+x = "hola"
+y = "soy yo"
+# 2 formas distintas mismo resultado
+print(f"{x} {y}")
+print(x,y)
+
+#Entrada de datos
+
+#ip = input("Ingrese la direccion ip => ")
+#print("La direccion ip que has ingresado es: ", ip)
+
+#print("Ip Objetivo: " + input("Ingresa tu ip objetivo => "))
+
+#Imprimir string e int a la vez
+
+x = "Can"
+y = 3 
+print(str(y) + x)
+
+x = "6"
+y = 3 
+print(int(x) + y)
+
+# Primera tare
+
+first_name = input("Cual es tu nombre? ")
+last_name = input("Cual es tu apellido? ")
+#print("Tu nombre es: ",first_name, last_name)
+print(f"Tu nombre es: {first_name} {last_name} ")
