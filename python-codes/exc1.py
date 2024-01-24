ae98ce22cad15419d3c9923af847953fb2a76e24
brai-lrnz <elibraibel@gmail.com>
2024-01-24 16:45:22 -0400

    Dia 10

--- python-codes/exc1.py
+++ python-codes/exc1.py
@@ -5,4 +5,5 @@ print("Saludos!! Bienvenidos a mi programa")
 pet = input("Ingresa el nombre de tu mascota: ")
 city = input("Ingresa tu ciudad: ")
 
-print("Your new twitter handle and bio @cyber"+ pet + " " + "from",city)
+#print("Your new twitter handle and bio @cyber"+ pet + " " + "from",city)
+print(f"Your new twitter handle and bio @cyber{pet} from {city}")
---
+++ python-codes/forLoops.py
@@ -0,0 +1,9 @@
+
+frutas = ["manzana", "guineo", "melon", "pera"]
+for fruta in frutas:
+#    print("Yo como", fruta)
+    print(fruta)
+    
+for num in range (0, 100, 1):
+    if num % 3 == 0:
+        print(num)
