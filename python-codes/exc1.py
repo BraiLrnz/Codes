fcbe0e8a3199a8fe03c41bed231c96734aa9acf7
BraiLrnz <99302867+BraiLrnz@users.noreply.github.com>
2026-01-06 16:38:47 -0400

    Delete python-codes directory

--- python-codes/exc1.py
+++
@@ -1,9 +0,0 @@
-# Primer Programa
-
-print("Saludos!! Bienvenidos a mi programa")
-
-pet = input("Ingresa el nombre de tu mascota: ")
-city = input("Ingresa tu ciudad: ")
-
-#print("Your new twitter handle and bio @cyber"+ pet + " " + "from",city)
-print(f"Your new twitter handle and bio @cyber{pet} from {city}")
--- python-codes/functions2.py
+++
@@ -1,7 +0,0 @@
-
-target = input("Ingrese la ip que desea escanear -> ")
-
-def scan (target):
-    print(f"[+] Escaneando la ip -> {target}")
-    
-scan(target)
